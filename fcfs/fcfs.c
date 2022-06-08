44d78e7999578cd469876ccfc76c8f3627d583e0
Sudip Das <sudip5428@gmail.com>
2022-06-08 23:30:17 +0530

    code commit for sjf implement in c

---
+++ fcfs/fcfs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#define MAX 30
+void main()
+{
+    float awt = 0, atat = 0;
+    int n, at[MAX], bt[MAX], i, j, wt[MAX];
+    printf("\n Enter the number of process:");
+    scanf("%d", &n);
+    printf("\n Enter the arrival time:");
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &at[i]);
+        /* code */
+    }
+    printf("\n Enter the bus time:");
+    for (i = 0; i < n; i++)
+    {
+        /* code */
+        scanf("%d", &bt[i]);
+    }
+}
