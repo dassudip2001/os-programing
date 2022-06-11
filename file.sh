cc26370fbe86b72f4dd8d0f27c7458c65f284cd8
Sudip Das <sudip5428@gmail.com>
2022-06-11 20:37:36 +0530

    code commit for file program

---
+++ file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash  
+  
+File=read_file.txt  
+if [ -f "$File" ]; then  
+echo "$File exist"  
+else  
+echo "$File does not exist"  
+fi
---
+++ sjf/sjf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define MAX 30
+void main()
+{
+    int i, j, n, t, p[MAX], bt[MAX], wt[MAX], tat[MAX];
+    float awt = 0, atat = 0;
+}
